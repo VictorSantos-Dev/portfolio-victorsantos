21d39a80f1eaa9382b2a5a41236f80d8a312efc7
Victor Santos <victor.hugo@itpower.com.br>
2023-08-25 13:38:37 -0300

    add todo list com bubble no portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/gigagames.jpg'
 import IMG2 from '../../assets/doguinhofeliz.jpg'
-import IMG3 from '../../assets/todolist.png'
-import IMG4 from '../../assets/CakeBoss.png'
+import IMG3 from '../../assets/CakeBoss.png'
+import IMG4 from '../../assets/todolist.png'
+import IMG5 from '../../assets/ToDoList_Bubble.jpg'
 import {FaGithub} from 'react-icons/fa'
 
 
@@ -47,7 +48,19 @@ const Portfolio = () => {
 
                 <article className='portfolio__item'>
                     <div className="portfolio__item-image">
-                        <img src={IMG3} height="370px" alt="" />
+                        <img src={IMG3} height="370px" alt="CakeBoss" />
+                    </div>
+                    <h4>CakeBoss</h4>
+                    <p className='small_text'>Website para uma confeitaria com sistema de agendamentos de entrega/retirada e carrinho de compras feito com Bootstrap e PHP + MySQL.</p>
+                    <div className="portfolio__item-cta">
+                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn' target="_blank"><FaGithub/></a>
+                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn btn-primary' target="_blank">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={IMG4} height="370px" alt="" />
                     </div>
                     <h4>ToDo List</h4>
                     <p className='small_text'>Lista de tarefas para anotações feita com ReactJs.
@@ -61,13 +74,14 @@ const Portfolio = () => {
 
                 <article className='portfolio__item'>
                     <div className="portfolio__item-image">
-                        <img src={IMG4} height="370px" alt="CakeBoss" />
+                        <img src={IMG5} height="370px" alt="" />
                     </div>
-                    <h4>CakeBoss</h4>
-                    <p className='small_text'>Website para uma confeitaria com sistema de agendamentos de entrega/retirada e carrinho de compras feito com Bootstrap e PHP + MySQL.</p>
+                    <h4>Lista de Produtos</h4>
+                    <p className='small_text'>Lista de compras de produtos feito com Bubble (Version Test).
+                    </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://bubble.io' className='btn' target="_blank"><FaGithub/></a>
+                        <a href='https://firstproject-70149.bubbleapps.io/version-test/listamercado_login' className='btn btn-primary' target="_blank">Live Demo</a>
                     </div>
                 </article>
             </div>
