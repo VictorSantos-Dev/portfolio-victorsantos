10612925e4bb0821dbb392a972c5d122d61d1226
Victor Santos <victor.hugo@itpower.com.br>
2023-08-29 14:03:33 -0300

    norefer

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocial = () => {
   return (
     <div className='header__social'>
-        <a href='https://www.linkedin.com/in/victorsantos-dev/' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com/VictorSantos-Dev' target="_blank"><FaGithub/></a>
+        <a href='https://www.linkedin.com/in/victorsantos-dev/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href='https://github.com/VictorSantos-Dev' target="_blank" rel="noreferrer"><FaGithub/></a>
     </div>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -25,8 +25,8 @@ const Portfolio = () => {
                         Criado para o TCC (Trabalho de Conlusão de Curso).
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/VictorSantos-Dev/gigagames.mytcc.com.br' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='http://www.gigagames.mytcc.com.br/home/' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://github.com/VictorSantos-Dev/gigagames.mytcc.com.br' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='http://www.gigagames.mytcc.com.br/home/' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                     </div>
                 </article>
 
@@ -40,8 +40,8 @@ const Portfolio = () => {
                         Colabaroção no desenvolvimento do TCC (Trabalho de Conlusão de Curso) de um colega de classe.
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/gsantos20/DoguinhoFeliz' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://doguinhofeliz.mytcc.com.br' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://github.com/gsantos20/DoguinhoFeliz' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='https://doguinhofeliz.mytcc.com.br' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                     </div>
                 </article>
 
@@ -56,8 +56,8 @@ const Portfolio = () => {
                         CRUD para bolos, sistema de login e etc...
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='https://drive.google.com/drive/folders/1HNmXQSvGdeqbX6pgHFB1ZTTryHjqAI1V?usp=sharing' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                     </div>
                 </article>
 
@@ -69,8 +69,8 @@ const Portfolio = () => {
                     <p className='small_text'>Landing page responsiva de um banco digital fictício chamado Victor's Bank. Feito com React.
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/VictorSantos-Dev/landingpage_victorsbank' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://landingpage-victorsbank.vercel.app' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://github.com/VictorSantos-Dev/landingpage_victorsbank' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='https://landingpage-victorsbank.vercel.app' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                     </div>
                 </article>
 
@@ -82,8 +82,8 @@ const Portfolio = () => {
                     <p className='small_text'>Lista de tarefas para anotações feita com ReactJs + Vite.
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://github.com/VictorSantos-Dev/todo_react' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://todo-react-teal-two.vercel.app' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://github.com/VictorSantos-Dev/todo_react' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='https://todo-react-teal-two.vercel.app' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                         {/* <a href='' className='btn btn-primary disabled' target="_blank">Em construção</a> */}
                     </div>
                 </article>
@@ -96,8 +96,8 @@ const Portfolio = () => {
                     <p className='small_text'>Lista de anotações feito com Bubble (Version Test).
                     </p>
                     <div className="portfolio__item-cta">
-                        <a href='https://bubble.io/page?type=page&name=listamercado_login&id=firstproject-70149&tab=tabs-1' className='btn' target="_blank"><FaGithub/></a>
-                        <a href='https://firstproject-70149.bubbleapps.io/version-test/listamercado_login' className='btn btn-primary' target="_blank">Live Demo</a>
+                        <a href='https://bubble.io/page?type=page&name=listamercado_login&id=firstproject-70149&tab=tabs-1' className='btn' target="_blank" rel="noreferrer"><FaGithub/></a>
+                        <a href='https://firstproject-70149.bubbleapps.io/version-test/listamercado_login' className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
                     </div>
                 </article>
             </div>
